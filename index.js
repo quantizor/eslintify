b203e9c918e9f2aefafde4af80cd89ae294fc7b7
Evan Jacobs <evan.jacobs@enigma.io>
2016-05-30 00:33:09 -0400

    Add quiet mode via `--quiet` flag
    The implementation exactly matches how ESLint handles this
    behavior in their CLI.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var eslint = require('eslint');
 var through = require('through');
 var error = console.error.bind(console);
+var default_formatter = require.resolve('eslint-friendly-formatter');
+
+function hasCount(result) {
+    return result.errorCount || result.warningCount;
+}
 
 function lint(file, options) {
     options = options || {};
+    options.formatter = options.formatter || default_formatter;
 
     var cli = new eslint.CLIEngine(options);
-    var formatter = cli.getFormatter(options.formatter || require.resolve('eslint-friendly-formatter'));
+    var formatter = cli.getFormatter(options.formatter);
     var data = '';
 
     function write(buf) {
@@ -16,13 +22,16 @@ function lint(file, options) {
     function end() {
         var results = cli.executeOnText(data, file).results;
 
-        if (results.length && results.some(function (r) { return r.errorCount || r.warningCount; })) {
-            error(formatter(results));
-        }
+        if (options.quiet) {
+            results = eslint.CLIEngine.getErrorResults(results);
+        } // filter out warnings
 
-        var errorResults = eslint.CLIEngine.getErrorResults(results);
-        if (errorResults.length && !options.continuous) {
-            this.emit('error', 'eslintify: linting error(s) detected.');
+        if (results.length && results.some(hasCount)) {
+            error(formatter(results));
+
+            if (!options.continuous) {
+                this.emit('error', 'eslintify: linting error(s) detected.');
+            }
         }
 
         this.queue(data);
@@ -36,8 +45,13 @@ function lint(file, options) {
     var givenExtension = file.slice(file.lastIndexOf('.') + 1, file.length);
     var supportedExtensions = ['js', 'jsx', 'es6'];
 
-    if (options.extension) { supportedExtensions = supportedExtensions.concat(options.extension); }
-    if (options.extensions) { supportedExtensions = supportedExtensions.concat(options.extensions); }
+    if (options.extension) {
+        supportedExtensions = supportedExtensions.concat(options.extension);
+    }
+
+    if (options.extensions) {
+        supportedExtensions = supportedExtensions.concat(options.extensions);
+    }
 
     if (supportedExtensions.indexOf(givenExtension) !== -1) {
         return through(write, end);
